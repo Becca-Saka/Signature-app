62d7a74a5a56f04fc72b536bb7e1557c30cd008d
Becca-Saka <Ree4sure@gmail.com>
2021-03-22 23:37:05 +0100

    snackbar added

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:signature_app/signature.dart';
 
-
-
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -20,4 +17,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
--- lib/painter.dart
+++ lib/painter.dart
@@ -1,38 +1,28 @@
-
 import 'package:flutter/material.dart';
 
-class SignatureCanva extends CustomPainter{
+class SignatureCanva extends CustomPainter {
   final List<Offset> points;
   final double width;
   final Color color;
-  
 
-  SignatureCanva({this.points,  this.width, this.color});
-
-  
-  
+  SignatureCanva({this.points, this.width, this.color});
 
   @override
   void paint(Canvas canvas, Size size) {
+    Paint paint = new Paint()
+      ..color = color
+      ..strokeCap = StrokeCap.round
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = width;
 
-     Paint paint = new Paint()
-     ..color = color
-     ..strokeCap = StrokeCap.round
-     ..style = PaintingStyle.stroke
-     ..strokeWidth = width;
-     
-
-
-    for(int i = 0; i < points.length - 1; i++ ) {
-
-      if(points[i] != null && points[i + 1] != null) {
+    for (int i = 0; i < points.length - 1; i++) {
+      if (points[i] != null && points[i + 1] != null) {
         canvas.drawLine(points[i], points[i + 1], paint);
-
       }
     }
   }
 
   @override
-  bool shouldRepaint(SignatureCanva oldDelegate)=>oldDelegate.points != points;
-
+  bool shouldRepaint(SignatureCanva oldDelegate) =>
+      oldDelegate.points != points;
 }
